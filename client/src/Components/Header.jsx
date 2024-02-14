8d1da70c82b420bce121aecf12424172203b71be
vashishtkumar <vashishtk270@gmail.com>
2024-02-14 12:29:51 +0530

    Header.jsx updated

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
 
            <Link to="/">
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-        <span className="text-slate-500">Reactor</span>
-        <span className="text-slate-700">Estate</span>
+        <span className="text-slate-500">Urban</span>
+        <span className="text-slate-700">Nest</span>
         </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center '>
