c8de0fe38a0aefa5ccd6341d9f24bee14f4e990d
vashishtkumar <vashishtk270@gmail.com>
2024-02-25 23:58:02 +0530

    temp_home_about

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
-    </div>
+    <>
+  <p className='bg-green-500'>
+  At the heart of our project lies a vision crafted by a team of innovative minds: Vashisht Kumar, the strategic thinker known for his groundbreaking approaches to sustainable development; Varin Bali, the creative genius whose designs reimagine living spaces; Vivekanand Rohtsavi, the analytical powerhouse with a knack for market trends; and Vansh Rana, the operational wizard who turns visions into reality. Together, we envision a real estate ecosystem that transcends traditional boundaries, creating not just homes, but sustainable communities where every detail reflects our commitment to excellence, innovation, and the betterment of society. Our journey is fueled by a desire to redefine the real estate landscape, ensuring every project we undertake is a step towards a more sustainable, inclusive, and innovative future.
+</p>
+
+</>
   )
 }
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      Home
+      {/*top*/}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+      <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>Find your next 
+       <span className='text-slate-500 '> Perfect</span> 
+      <br/>
+      Place with ease
+      </h1>
+      <div className="text-gray-400 text-xs sm:text-sm">
+        Urban nest is the best place to find your next 
+        perfext place to live
+        <br/>
+       we have a wide range of properties for you to choose from.
+      </div>
+      <Link to="/search" className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'>
+       Lets get started ...
+      </Link>
+    </div>
+
+    {/* swiper */}
+     
+     <img src="https://media.istockphoto.com/id/174911746/nl/foto/business-district.jpg?s=1024x1024&w=is&k=20&c=_d84RatAISDi6xJmMvVUtr2l3R-W8KzJR9n2bMS4iY8=" alt="building" width='100%'/>
+
     </div>
   )
 }
