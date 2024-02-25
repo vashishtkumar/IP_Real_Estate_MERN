ca09efff94a52e63e6e392540b5f4f950db46f6c
vashishtkumar <vashishtk270@gmail.com>
2024-02-25 22:42:41 +0530

    New commit message

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,23 +1,23 @@
+
 import mongoose from "mongoose";
 
-const userSchema= new mongoose.schema({
-username:{
-    type:String,
-    required : true,
-    unique:true,
-},
-email:{
-    type:String,
-    required : true,
-    unique:true,
-},
-password:{
-    type:String,
-    required : true,
-},
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
-},{timestamps: true });
+const User = mongoose.model('User', userSchema);
 
-const user =mongoose.model('user',userSchema);
-
-export default user;
+export default User;
