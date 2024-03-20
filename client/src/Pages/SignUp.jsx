600634afe4bb8b5c1bd53f6cfa23b3d76dbeb788
Vrohs <info.rohsgaming536@gmail.com>
2024-03-20 10:18:59 +0530

    Refactor SignUp component

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import {Link} from 'react-router-dom'
 
 export default function SignUp() {
+  
+
+
+
+  const handleChange=(e)=>{
+
+  }
   return (
-    <div>
-      <h1 className="text-3xl text-center font-semibold my-7 ">Sign Up</h1>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>
+      Sign Up</h1>
+      <form  className='flex flex-col gap-4 '>
+        <input type="text" placeholder='username'
+         className='border p-3 rounded-lg' id='username'onChange={handleChange}/>
 
-      <form action=" ">
-        <input type="text" placeholder="username" 
-        className="border p-3 rounded-lg" id="username" />
+<input type="email" placeholder='email'
+         className='border p-3 rounded-lg' id='email'onChange={handleChange}/>
 
-      <input type="text" placeholder="username" 
-        className="border p-3 rounded-lg" id="username" />
-        
+<input type="password" placeholder='password'
+         className='border p-3 rounded-lg' id='password'onChange={handleChange}/>
+
+         <button className='bg-slate-700 text-white p-3
+          rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          SIGN UP</button>
       </form>
+      <div className='flex gap-2 mt-5'>
+       <p>Have an account?</p>
+       <Link to={"/sign-in"}>
+        <span className='text-blue-700 '>Sign in</span>
+       </Link>
+      </div>
     </div>
-  );
+  )
 }
