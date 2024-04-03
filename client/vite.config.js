12d4eed0079733fa22fe812e242bcb0e3c96cf0a
vashishtkumar <vashishtk270@gmail.com>
2024-04-03 12:40:56 +0530

    complete signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3000',
         secure: false,
 
     },
