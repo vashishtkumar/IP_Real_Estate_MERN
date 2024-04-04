eb60cc7af50e6f5dc0307a43c9882b9e15debb31
vashishtkumar <vashishtk270@gmail.com>
2024-04-04 23:35:04 +0530

    add redux tool kit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
-import Signin from './Pages/Signin';
 import SignUp from './Pages/SignUp';
 import About from './Pages/About';
 import Profile from './Pages/Profile';
 import Header from './Components/Header';
+import SignIn from './Pages/Signin';
 
 export default function App() {
 
@@ -13,7 +13,7 @@ return <BrowserRouter>
   <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/sign-in" element={<Signin/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/profile" element={<Profile/>}/>
