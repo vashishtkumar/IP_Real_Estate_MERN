2ea0ae4175abb3841c691eba6ae292fa64261555
vashishtkumar <vashishtk270@gmail.com>
2024-04-04 19:23:18 +0530

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,19 @@
-import express from 'express'
-import {signup} from '../controllers/auth.controller.js';
+// import express from 'express'
+// import {signup} from '../controllers/auth.controller.js';
 
-const router =express.Router();
+// const router =express.Router();
 
-router.post("/signup",signup);
+// router.post("/signup",signup);
+
+// export default router;
+
+
+import express from 'express';
+import { signin, signup } from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
