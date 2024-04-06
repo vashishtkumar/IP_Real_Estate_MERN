e962670417ed9f10489d3632ddcc47da1a19455d
vashishtkumar <vashishtk270@gmail.com>
2024-04-06 17:43:09 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,37 +1,36 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
-mongoose.connect(process.env.MONGO)
-.then(()=>{
-    console.log('connected to MOngoDB!!');
-})
-.catch((err)=>{
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
     console.log(err);
-})
-
-
-const app=express();
+  });
+const app = express();
 
 app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000!!!!");
-}
-);
+app.use(cookieParser());
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-
-app.use((err,req,res,next)=>{
-const statusCode=err.statusCode || 500;
-const message= err.message || 'Internal server Error';
-
-return res.status(statusCode).json({
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-});
+  });
 });
